82cc31a494cbd15f79421ba6d33bb888c7563c1d
Christian Veiby <chriveib@stud.ntnu.no>
2023-01-24 22:24:51 +0100

    Authscreen created

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,19 @@
-// client/src/App.js
-
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Auth from "./components/Auth"
+import background from "./images/comeon.jpg"
 
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+    <div style={{ backgroundImage: `url(${background})` }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ client/src/components/Auth.js
@@ -0,0 +1,86 @@
+
+import React, { useState } from "react"
+
+export default function (props) {
+  let [authMode, setAuthMode] = useState("signin")
+
+  const changeAuthMode = () => {
+    setAuthMode(authMode === "signin" ? "signup" : "signin")
+  }
+
+  if (authMode === "signin") {
+    return (
+      <div className="Auth-form-container">
+        <form className="Auth-form">
+          <div className="Auth-form-content">
+            <h3 className="Auth-form-title">Logg inn</h3>
+            <div className="text-center">
+                Ny? {" "}
+              <span className="link-primary" onClick={changeAuthMode}>
+                registrer
+              </span>
+            </div>
+            <div className="form-group mt-3">
+              <label>Namn</label>
+              <input
+                type="text"
+                className="form-control mt-1"
+                placeholder="Fyll inn namn"
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Hemmelig ord</label>
+              <input
+                type="password"
+                className="form-control mt-1"
+                placeholder="Fyll inn passord"
+              />
+            </div>
+            <div className="d-grid gap-2 mt-3">
+              <button type="submit" className="btn btn-primary">
+                dønski
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    )
+  }
+
+  return (
+    <div className="Auth-form-container">
+      <form className="Auth-form">
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Registrer deg</h3>
+          <div className="text-center">
+            Har du bruker? {" "}
+            <span className="link-primary" onClick={changeAuthMode}>
+              logg inn
+            </span>
+          </div>
+          <div className="form-group mt-3">
+            <label>Namn</label>
+            <input
+              type="text"
+              className="form-control mt-1"
+              placeholder="e.g Hugh Hefner"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Hemmelig ord - obs! passord blir ikke hashet </label>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="passord blir ikke hashet"
+            />
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              dønski
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
+}
